c287115c3f3c506db8c028eeeb8daf2633893b0c
joycewambui <jwambui@protonmail.com>
2019-01-21 15:42:47 +0300

    Updates

---
+++ precache-manifest.0970bbd0e4c1c1f72588ddcf3e1686d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8092386d88b17f8a88c5a298303e0751",
+    "url": "/What-Breed-Is-My-Cat/static/media/cat.8092386d.png"
+  },
+  {
+    "revision": "a6b6e961624a8677e6be",
+    "url": "/What-Breed-Is-My-Cat/static/js/runtime~main.a6b6e961.js"
+  },
+  {
+    "revision": "03ac182c9a4276988b75",
+    "url": "/What-Breed-Is-My-Cat/static/js/main.03ac182c.chunk.js"
+  },
+  {
+    "revision": "e78a5712a40370071368",
+    "url": "/What-Breed-Is-My-Cat/static/js/1.e78a5712.chunk.js"
+  },
+  {
+    "revision": "03ac182c9a4276988b75",
+    "url": "/What-Breed-Is-My-Cat/static/css/main.9fbeaba3.chunk.css"
+  },
+  {
+    "revision": "e78a5712a40370071368",
+    "url": "/What-Breed-Is-My-Cat/static/css/1.5416a2f8.chunk.css"
+  },
+  {
+    "revision": "debeca20c24180a3a25d66d84f7ac862",
+    "url": "/What-Breed-Is-My-Cat/index.html"
+  }
+];
